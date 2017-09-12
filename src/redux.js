497728a07254127c4bd187a0f03e9250f5cbf9d8
jonathan schatz <jon@instamotor.com>
2017-09-12 14:00:46 -0700

    simplify reducer

--- src/redux.js
+++ src/redux.js
@@ -1,36 +1,34 @@
-import { breakPoints as defaultBreakPoints } from './defaults'
+import { breakPoints } from './defaults'
 
 const SET_MOBILE_DETECT = '@@react-responsive-redux/SET_MOBILE_DETECT'
 
 export const setMobileDetect = ({ phone, tablet, mobile, desktop } = {}) => ({ type: SET_MOBILE_DETECT, phone, tablet, mobile, desktop })
 
-export const buildResponsiveReducer = () => {
-  // TODO - allow users to pass this in - we have to share it with our components
-  // too though so maybe we need a getter/setter on our entire class?
+// TODO - allow users to pass this in - we have to share it with our components
+// too though so maybe we need a getter/setter on our entire class?
 
-  const breakPoints = defaultBreakPoints
-  // default to a desktop size if in doubt
-  const defaultSize = defaultBreakPoints.tablet + 1
+// default to a desktop size if in doubt
+const defaultSize = breakPoints.tablet + 1
 
-  const initialState = {
-    phone: false,
-    tablet: false,
-    mobile: false,
-    fakeWidth: defaultSize,
-  }
+const initialState = {
+  phone: false,
+  tablet: false,
+  mobile: false,
+  fakeWidth: defaultSize,
+}
 
-  return (state = initialState, action) => {
-    switch (action.type) {
-      case SET_MOBILE_DETECT: {
-        const { mobile, tablet, phone, desktop } = action
+export const responsiveReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_MOBILE_DETECT: {
+      const { mobile, tablet, phone, desktop } = action
+      let fakeWidth
 
-        let fakeWidth
-        if (mobile) {
-          if (phone) {
-            fakeWidth = breakPoints.phone
-          } else if (tablet) {
-            fakeWidth =  breakPoints.tablet
-          } else {
+      if (mobile) {
+        if (phone) {
+          fakeWidth = breakPoints.phone
+        } else if (tablet) {
+          fakeWidth =  breakPoints.tablet
+        } else {
             // TODO - should we ever get here? default to the lowest value i guess
             fakeWidth = breakPoints.phone
           }
@@ -41,6 +39,5 @@ export const buildResponsiveReducer = () => {
       }
       default:
         return state
-    }
   }
 }
