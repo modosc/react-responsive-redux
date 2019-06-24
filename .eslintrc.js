9741ca44848ba08188a55eb810d40f85cecaff7c
jonathan schatz <jon@divisionbyzero.com>
2019-06-24 09:11:42 -0700

    first pass

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,5 +44,7 @@ module.exports = {
     'react/jsx-no-literals': 0,
     'import/prefer-default-export': 0,
     'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 0,
+    'unicorn/prevent-abbreviations': 0,
   },
 }
